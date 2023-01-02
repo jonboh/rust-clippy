c298950ac2ee6bf8c6a3216bc2b660e0b5d0871e
chansuke <moonset20@gmail.com>
2023-01-02 23:25:08 +0900

    refactor: delete unnecessary comparison squashed changes by chansuke
    refactor: fix re-daclare logic
    
    chore: run `cargo dev update_lints`
    
    hotfix: replace `check_block` with `check_expr`
    
    hotfix: prevent linting raw int
    
    refactor: use `rustc_span::sym` for method name comparison
    
    hotfix: remove unnecessary path
    
    hotfix: update declare_lints name
    
    refactor: update MSRV constant name to follow naming convention
    
    hotfix: rollback declare lints name
    
    hotfix: add type annotation to let binding
    
    hotfix: deduplicate registration

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -996,6 +996,7 @@ pub fn register_plugins(store: &mut rustc_lint::LintStore, sess: &Session, conf:
     store.register_late_pass(|_| Box::new(manual_slice_size_calculation::ManualSliceSizeCalculation));
     store.register_early_pass(|| Box::new(suspicious_doc_comments::SuspiciousDocComments));
 <<<<<<< HEAD
+<<<<<<< HEAD
     let excessive_nesting_threshold = conf.excessive_nesting_threshold;
     store.register_early_pass(move || {
         Box::new(excessive_nesting::ExcessiveNesting {
@@ -1068,7 +1069,7 @@ pub fn register_plugins(store: &mut rustc_lint::LintStore, sess: &Session, conf:
     });
     store.register_late_pass(move |_| Box::new(manual_hash_one::ManualHashOne::new(msrv())));
     store.register_late_pass(|_| Box::new(iter_without_into_iter::IterWithoutIntoIter));
-    store.register_late_pass(move || Box::new(unnecessary_reserve::UnnecessaryReserve::new(msrv)));
+    store.register_late_pass(move |_| Box::new(unnecessary_reserve::UnnecessaryReserve::new(msrv())));
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
 
--- clippy_lints/src/unnecessary_reserve.rs
+++ clippy_lints/src/unnecessary_reserve.rs
@@ -1,18 +1,21 @@
 use clippy_utils::diagnostics::span_lint_and_then;
-use clippy_utils::{match_def_path, meets_msrv, msrvs, paths, visitors::expr_visitor_no_bodies};
+use clippy_utils::msrvs::{self, Msrv};
+use clippy_utils::{get_enclosing_block, match_def_path, paths, visitors::for_each_expr, SpanlessEq};
+use core::ops::ControlFlow;
 use rustc_errors::Applicability;
-use rustc_hir::{intravisit::Visitor, Block, ExprKind, QPath, StmtKind};
+use rustc_hir::{Block, Expr, ExprKind, PathSegment};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_middle::ty;
-use rustc_semver::RustcVersion;
 use rustc_session::{declare_tool_lint, impl_lint_pass};
 use rustc_span::sym;
 
 declare_clippy_lint! {
     /// ### What it does
+    ///
     /// This lint checks for a call to `reserve` before `extend` on a `Vec` or `VecDeque`.
     /// ### Why is this bad?
     /// Since Rust 1.62, `extend` implicitly calls `reserve`
+    ///
     /// ### Example
     /// ```rust
     /// let mut vec: Vec<usize> = vec![];
@@ -32,33 +35,28 @@ declare_clippy_lint! {
     "calling `reserve` before `extend` on a `Vec` or `VecDeque`, when it will be called implicitly"
 }
 
-pub struct UnnecessaryReserve {
-    msrv: Option<RustcVersion>,
-}
+impl_lint_pass!(UnnecessaryReserve => [UNNECESSARY_RESERVE]);
 
+pub struct UnnecessaryReserve {
+    msrv: Msrv,
+}
 impl UnnecessaryReserve {
-    #[must_use]
-    pub fn new(msrv: Option<RustcVersion>) -> Self {
+    pub fn new(msrv: Msrv) -> Self {
         Self { msrv }
     }
 }
 
-impl_lint_pass!(UnnecessaryReserve => [UNNECESSARY_RESERVE]);
-
 impl<'tcx> LateLintPass<'tcx> for UnnecessaryReserve {
-    fn check_block(&mut self, cx: &LateContext<'tcx>, block: &Block<'tcx>) {
-        if !meets_msrv(self.msrv, msrvs::UNNECESSARY_RESERVE) {
+    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &Expr<'tcx>) {
+        if !self.msrv.meets(msrvs::EXTEND_IMPLICIT_RESERVE) {
             return;
         }
 
-        for (idx, stmt) in block.stmts.iter().enumerate() {
-            if let StmtKind::Semi(semi_expr) = stmt.kind
-                && let ExprKind::MethodCall(_, [struct_calling_on, _], _) = semi_expr.kind
-                && let Some(expr_def_id) = cx.typeck_results().type_dependent_def_id(semi_expr.hir_id)
-                && (match_def_path(cx, expr_def_id, &paths::VEC_RESERVE) ||
-                    match_def_path(cx, expr_def_id, &paths::VEC_DEQUE_RESERVE))
+        if let ExprKind::MethodCall(PathSegment { ident: method, .. }, struct_calling_on, args_a, _) = expr.kind
+                && method.name.as_str() == "reserve"
                 && acceptable_type(cx, struct_calling_on)
-                && let Some(next_stmt_span) = check_extend_method(cx, block, idx, struct_calling_on)
+                && let Some(block) = get_enclosing_block(cx, expr.hir_id)
+                && let Some(next_stmt_span) = check_extend_method(cx, block, struct_calling_on, &args_a[0])
                 && !next_stmt_span.from_expansion()
             {
                 span_lint_and_then(
@@ -68,7 +66,7 @@ impl<'tcx> LateLintPass<'tcx> for UnnecessaryReserve {
                     "unnecessary call to `reserve`",
                     |diag| {
                         diag.span_suggestion(
-                            semi_expr.span,
+                            expr.span,
                             "remove this line",
                             String::new(),
                             Applicability::MaybeIncorrect,
@@ -76,7 +74,6 @@ impl<'tcx> LateLintPass<'tcx> for UnnecessaryReserve {
                     }
                 );
             }
-        }
     }
 
     extract_msrv_attr!(LateContext);
@@ -97,55 +94,28 @@ fn acceptable_type(cx: &LateContext<'_>, struct_calling_on: &rustc_hir::Expr<'_>
 fn check_extend_method(
     cx: &LateContext<'_>,
     block: &Block<'_>,
-    idx: usize,
     struct_expr: &rustc_hir::Expr<'_>,
+    args_a: &rustc_hir::Expr<'_>,
 ) -> Option<rustc_span::Span> {
+    let args_a_kind = &args_a.kind;
     let mut read_found = false;
-    let next_stmt_span;
+    let mut spanless_eq = SpanlessEq::new(cx);
 
-    let mut visitor = expr_visitor_no_bodies(|expr| {
-        if let ExprKind::MethodCall(_, [struct_calling_on, _], _) = expr.kind
+    let _: Option<!> = for_each_expr(block, |expr| {
+        if let ExprKind::MethodCall(_, struct_calling_on, _,_) = expr.kind
             && let Some(expr_def_id) = cx.typeck_results().type_dependent_def_id(expr.hir_id)
+            && let ExprKind::MethodCall(PathSegment { ident: method_call_a, .. },..) = args_a_kind
+            && method_call_a.name == rustc_span::sym::len
             && match_def_path(cx, expr_def_id, &paths::ITER_EXTEND)
             && acceptable_type(cx, struct_calling_on)
-            && equal_ident(struct_calling_on, struct_expr)
+            // Check that both expr are equal
+            && spanless_eq.eq_expr(struct_calling_on, struct_expr)
         {
             read_found = true;
         }
-        !read_found
+        let _: bool = !read_found;
+        ControlFlow::Continue(())
     });
 
-    if idx == block.stmts.len() - 1 {
-        if let Some(e) = block.expr {
-            visitor.visit_expr(e);
-            next_stmt_span = e.span;
-        } else {
-            return None;
-        }
-    } else {
-        let next_stmt = &block.stmts[idx + 1];
-        visitor.visit_stmt(next_stmt);
-        next_stmt_span = next_stmt.span;
-    }
-    drop(visitor);
-
-    if read_found {
-        return Some(next_stmt_span);
-    }
-
-    None
-}
-
-#[must_use]
-fn equal_ident(left: &rustc_hir::Expr<'_>, right: &rustc_hir::Expr<'_>) -> bool {
-    fn ident_name(expr: &rustc_hir::Expr<'_>) -> Option<rustc_span::Symbol> {
-        if let ExprKind::Path(QPath::Resolved(None, inner_path)) = expr.kind
-            && let [inner_seg] = inner_path.segments
-        {
-            return Some(inner_seg.ident.name);
-        }
-        None
-    }
-
-    ident_name(left) == ident_name(right)
+    if read_found { Some(block.span) } else { None }
 }
